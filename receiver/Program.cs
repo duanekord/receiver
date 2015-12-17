8792a8194558d7daff4d82c821d207b9b52b5168
Duane Kord <duanekord@outlook.com>
2015-12-17 14:20:45 -0800

    Project Begin

---
+++ receiver/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+
+namespace receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eventHubConnectionString = "Endpoint=sb://pendletoneventhub-ns.servicebus.windows.net/;SharedAccessKeyName=ReceiveRule;SharedAccessKey=+UB+Fak8rj/JLet3mhOe/fbM1B8tYhAvSN6wwqUW32s=";
+            string eventHubName = "pendletoneventhub";
+            string storageAccountName = "duanekord";
+            string storageAccountKey = "XlLxQZ80siskOjCvd0p5X17oRB3hyiQ28T+cGF1/HlZvYggnQR0qCFDfEPpqd+cVV48MLcYznaKpQn1X6n+YGw==";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+            string eventProcessorHostName = Guid.NewGuid().ToString();
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+            Console.WriteLine("Registering EventProcessor...");
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>().Wait();
+
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+    }
+}
